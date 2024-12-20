0f8fcbfc2b177989e85d1d884fd33ad3384a52bf
Samarth-T17 <samarthturmari@gmail.com>
2024-12-20 09:37:07 +0530

    before test 2

--- customOperations/archBuilder/archBuilder.py
+++ customOperations/archBuilder/archBuilder.py
@@ -1,9 +1,13 @@
 from base_arch.LakdNet import Mixerblock as LakdBlock
-from base_arch.fftFormer import TransformerBlock as FftBlock
-from base_arch.Loformer import TransformerBlock_2b as loformerblock
+from base_arch.FftFormer import TransformerBlock as FftBlock
+from base_arch.Loformer import TransformerBlock_2b as LoformerBlock
 from base_arch.Nafnet import NAFBlock
+from base_arch.CGNet import NAFBlock0 as CGNafnet, CascadedGazeBlock as CGBlock
+from base_arch.Restormer import TransformerBlock as RestormerBlock
+from base_arch.CaptNet import TransformerBlock as CaptBlock, CNNBlock as CaptCNNBlock
 import torch
 import torch.nn as nn
+
 def nafnet_builder(params):
     dim = params[3] * (2 ** params[2])
     param1 = int(params[0])
@@ -28,8 +32,6 @@ def lakd_builder(params):
     dim = params[3] * (2 ** params[2])
     param1 = int(params[0])
     param2 = int(params[1] * 2 + 1)
-    print(param1, param2, dim
-          )
     if params[2] != 3:
         return [nn.Sequential(*[LakdBlock(dim=dim, mix_kernel_size=param2, ffn_expansion_factor=2.3,
                                            bias=False, LayerNorm_type='WithBias') for i in range(param1)]),
@@ -45,15 +47,43 @@ def UFP_builder(params):
 
 
 def CG_builder(params):
-    pass
+    dim = params[3] * (2 ** params[2])
+    param1 = int(params[0])
+    param2 = int(params[1] * 2 + 1)
+    if params[2] != 3:
+        return [nn.Sequential(CGBlock(dim, GCE_Conv=param2), *[CGNafnet(dim) for i in range(param1 - 1)]),
+                nn.Sequential(*[CGNafnet(dim) for i in range(param1)])]
+    else :
+        return [nn.Sequential(*[CGNafnet(dim) for i in range(param1)])]
+
 
 
 def Capt_builder(params):
-    pass
+    dim = params[3] * (2 ** params[2])
+    param1 = int(params[0])
+    param2 = int(params[1] * 2 + 1)
+    if params[2] != 3:
+        return [nn.Sequential(*([CaptCNNBlock(dim) for _ in range(param1)] if params[2] <= 1
+        else [CaptBlock(dim, num_heads=param2) for _ in range(param1)])),
+                nn.Sequential(*[CGNafnet(dim) for i in range(param1)])]
+    else :
+        return [nn.Sequential(*[CGNafnet(dim) for i in range(param1)])]
+
 
 
 def Rest_builder(params):
-    pass
+    dim = params[3] * (2 ** params[2])
+    param1 = int(params[0])
+    param2 = int(params[1] * 2 + 1)
+    if params[2] != 3:
+        return [nn.Sequential(*[RestormerBlock(dim=dim, num_heads=param2, ffn_expansion_factor=2.66, bias=False,
+                LayerNorm_type='WithBias') for i in range(param1)]),
+                nn.Sequential(*[RestormerBlock(dim=dim, num_heads=param2, ffn_expansion_factor=2.66, bias=False,
+                                               LayerNorm_type='WithBias') for i in range(param1)])]
+    else :
+        return [nn.Sequential(*[RestormerBlock(dim=dim, num_heads=param2, ffn_expansion_factor=2.66, bias=False,
+                LayerNorm_type='WithBias') for i in range(param1)])]
+
 
 def Swin_builder(params):
     pass
@@ -66,33 +96,33 @@ def Lo_builder(params):
     if params[2] != 3:
         return [
             nn.Sequential(
-            *[loformerblock(dim=dim, num_heads=param2, ffn_expansion_factor=2.66,
+            *[LoformerBlock(dim=dim, num_heads=param2, ffn_expansion_factor=2.66,
+                            bias=True, LayerNorm_type='WithBias',
+                            window_size=8, window_size_dct=8,
+                            num_k=8,
+                            cs=['channel_mlp', 'channel_mlp'], norm_type_=[['LayerNorm', 'LayerNorm'], ['LayerNorm', 'LayerNorm']],
+                            qk_norm=[False, False], temp_adj=None,
+                            i=None, ffn='ffn') for _ in range(param1)]),
+            nn.Sequential(
+            *[LoformerBlock(dim=dim, num_heads=param2, ffn_expansion_factor=2.66,
+                            bias=True, LayerNorm_type='WithBias',
+                            window_size=8, window_size_dct=8,
+                            num_k=8,
+                            cs=['channel_mlp', 'channel_mlp'],
+                            norm_type_=[['LayerNorm', 'LayerNorm'], ['LayerNorm', 'LayerNorm']],
+                            qk_norm=[False, False], temp_adj=None,
+                            i=None, ffn='ffn') for _ in range(param1)])
+        ]
+
+    else :
+        return [nn.Sequential(
+                *[LoformerBlock(dim=dim, num_heads=param2, ffn_expansion_factor=2.66,
                                 bias=True, LayerNorm_type='WithBias',
                                 window_size=8, window_size_dct=8,
                                 num_k=8,
                                 cs=['channel_mlp', 'channel_mlp'], norm_type_=[['LayerNorm', 'LayerNorm'], ['LayerNorm', 'LayerNorm']],
                                 qk_norm=[False, False], temp_adj=None,
-                                i=None, ffn='ffn') for _ in range(param1)]),
-            nn.Sequential(
-            *[loformerblock(dim=dim, num_heads=param2, ffn_expansion_factor=2.66,
-                                bias=True, LayerNorm_type='WithBias',
-                                window_size=8, window_size_dct=8,
-                                num_k=8,
-                                cs=['channel_mlp', 'channel_mlp'],
-                                norm_type_=[['LayerNorm', 'LayerNorm'], ['LayerNorm', 'LayerNorm']],
-                                qk_norm=[False, False], temp_adj=None,
-                                i=None, ffn='ffn') for _ in range(param1)])
-        ]
-
-    else :
-        return [nn.Sequential(
-                *[loformerblock(dim=dim, num_heads=param2, ffn_expansion_factor=2.66,
-                                    bias=True, LayerNorm_type='WithBias',
-                                    window_size=8, window_size_dct=8,
-                                    num_k=8,
-                                    cs=['channel_mlp', 'channel_mlp'], norm_type_=[['LayerNorm', 'LayerNorm'], ['LayerNorm', 'LayerNorm']] ,
-                                    qk_norm=[False, False], temp_adj=None,
-                                    i=None, ffn='ffn') for _ in range(param1)]
+                                i=None, ffn='ffn') for _ in range(param1)]
             )]
 
 
---
+++ customOperations/archBuilder/base_arch/CaptNet.py
@@ -0,0 +1,154 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from utils.arch_utils import LayerNorm2d
+from einops import rearrange
+
+
+
+class SimpleGate(nn.Module):
+    def forward(self, x):
+        x1, x2 = x.chunk(2, dim=1)
+        return x1 * x2
+
+
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, ffn_expansion_factor=2, bias=True):
+        super(FeedForward, self).__init__()
+
+        hidden_features = int(dim * ffn_expansion_factor)
+
+        self.project_in = nn.Conv2d(dim, hidden_features * 2, kernel_size=1, bias=bias)
+        self.dwconv = nn.Conv2d(hidden_features * 2, hidden_features * 2, kernel_size=3, stride=1, padding=1,
+                                groups=hidden_features * 2, bias=bias)
+        self.project_out = nn.Conv2d(hidden_features, dim, kernel_size=1, bias=bias)
+        self.sg = SimpleGate()
+
+    def forward(self, x):
+        x = self.project_in(x)
+        x = self.dwconv(x)
+        x = self.sg(x)
+        x = self.project_out(x)
+        return x
+
+
+
+class Attention(nn.Module):
+    def __init__(self, dim, num_heads, is_prompt=False, bias=True):
+        super(Attention, self).__init__()
+        self.dim = dim
+        self.num_heads = num_heads
+        self.is_prompt = is_prompt
+        self.temperature = nn.Parameter(torch.ones(num_heads, 1, 1))
+        self.prompt = nn.Parameter(torch.ones(num_heads, dim//num_heads, 1))
+        self.qkv = nn.Conv2d(dim, dim * 3, kernel_size=1, bias=bias)
+        self.qkv_dwconv = nn.Conv2d(dim * 3, dim * 3, kernel_size=3, stride=1, padding=1, groups=dim * 3, bias=bias)
+        self.project_out = nn.Conv2d(dim, dim, kernel_size=1, bias=bias)
+
+    def with_prompt(self, tensor, prompt):
+        return tensor if prompt is None else tensor + prompt
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+
+        qkv = self.qkv_dwconv(self.qkv(x))
+        q, k, v = qkv.chunk(3, dim=1)
+
+        q = rearrange(q, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        k = rearrange(k, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        v = rearrange(v, 'b (head c) h w -> b head c (h w)', head=self.num_heads)
+        if self.is_prompt:
+            prompt = self.prompt
+            q = self.with_prompt(q, prompt)
+            k = self.with_prompt(k, prompt)
+            v = self.with_prompt(v, prompt)
+        q = torch.nn.functional.normalize(q, dim=-1)
+        k = torch.nn.functional.normalize(k, dim=-1)
+
+        attn = (q @ k.transpose(-2, -1)) * self.temperature
+        attn = attn.softmax(dim=-1)
+
+        out = (attn @ v)
+
+        out = rearrange(out, 'b head c (h w) -> b (head c) h w', head=self.num_heads, h=h, w=w)
+
+        out = self.project_out(out)
+        return out
+
+
+class TransformerBlock(nn.Module):
+    def __init__(self, dim, num_heads,prompt=False):
+        super(TransformerBlock, self).__init__()
+
+        self.norm1 = LayerNorm2d(dim)
+        self.attn = Attention(dim, num_heads, prompt)
+        self.norm2 = LayerNorm2d(dim)
+        self.ffn = FeedForward(dim)
+    def forward(self, x):
+        # x = self.norm1(x)
+        x = x + self.attn(self.norm1(x))
+        x = x + self.ffn(self.norm2(x))
+        return x
+
+
+class CNNBlock(nn.Module):
+    def __init__(self, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.):
+        super().__init__()
+        dw_channel = c * DW_Expand
+        self.conv1 = nn.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1,
+                               bias=True)
+        self.conv2 = nn.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1,
+                               groups=dw_channel,
+                               bias=True)
+        self.conv3 = nn.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1,
+                               groups=1, bias=True)
+
+        # Simplified Channel Attention
+        self.sca = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_channels=dw_channel // 2, out_channels=dw_channel // 2, kernel_size=1, padding=0, stride=1,
+                      groups=1, bias=True),
+        )
+
+        # SimpleGate
+        self.sg = SimpleGate()
+
+        ffn_channel = FFN_Expand * c
+        self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1,
+                               bias=True)
+        self.conv5 = nn.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1,
+                               groups=1, bias=True)
+
+        self.norm1 = LayerNorm2d(c)
+        self.norm2 = LayerNorm2d(c)
+
+        self.dropout1 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
+        self.dropout2 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
+
+        self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+        self.gamma = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+
+    def forward(self, inp):
+        x = inp
+
+        x = self.norm1(x)
+
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.sg(x)
+        x = x * self.sca(x)
+        x = self.conv3(x)
+
+        x = self.dropout1(x)
+
+        y = inp + x * self.beta
+
+        x = self.conv4(self.norm2(y))
+        x = self.sg(x)
+        x = self.conv5(x)
+
+        x = self.dropout2(x)
+
+        return y + x * self.gamma
